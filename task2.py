0f3fe2e2409450e37885b515b04c4afd67fe9e60
ZakirovaVera <zakirovavk@gmail.com>
2022-10-06 00:27:02 +0500

    Список простых множителей

--- task2.py
+++ task2.py
@@ -1,2 +1,14 @@
-# 2. Задайте натуральное число N. Напишите программу, 
+# 2. Задайте натуральное число N. Напишите программу,
 # которая составит список простых множителей числа N.
+
+num_user = int(input('Введите число: '))
+lst_multiplier = []
+index = 2
+
+while index <= num_user:
+    if num_user % index == 0:
+        lst_multiplier.append(index)
+        num_user //= index
+    else:
+        index += 1
+print(lst_multiplier)
